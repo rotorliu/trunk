a944b7619e3cf9e85d4fb645849f992949fd76c5
Daniel Girardeau-Montaut <daniel.girardeau@gmail.com>
2015-04-03 20:08:14 +0200

    Compilation with gcc (grrrrrr)

--- libs/qCC_glWindow/ccGLWindow.h
+++ libs/qCC_glWindow/ccGLWindow.h
@@ -685,7 +685,7 @@ protected:
 		ccAtomicBool m_pendingRedraw;
 	};
 
-	friend ccGLWindow::RenderingThread;
+	friend class RenderingThread;
 
 	RenderingThread* m_renderingThread;
 	QGLFormat m_format;
